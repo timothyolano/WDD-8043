8b676dc00a98ca0048acb6b59474216db4108cec
Martin Timothy M. Olano/ CPE 301 <89885436+timothyolano@users.noreply.github.com>
2022-09-30 21:31:17 +0800

    Add files via upload

---
+++ Exercise1.php
@@ -0,0 +1,25 @@
+<html> 
+<head><title> Exercise 1 </title></head>
+<body> 
+
+<?php
+ $fullname = "<b> Martin Timothy M. Olano </b>";
+ echo "My name is ".$fullname;
+ echo "<br>";
+ $collegeprog = "<b> Bachelor of Science in Computer Engineering (BS CpE) </b>";
+ echo "My College Program is".$collegeprog; 
+ echo "<br>";
+ $section = "<b> CPE 301 </b>";
+ echo "My Section is ".$section;
+ echo "<br>";
+ $string = "<b> Today, I will learn PHP programming </b>"; 
+ echo $string; 
+
+
+
+
+
+?>
+</body>
+</html> 
+
