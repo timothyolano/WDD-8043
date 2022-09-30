80df636c65b13996bcc0a9e3f559836a1d13a62c
Martin Timothy M. Olano/ CPE 301 <89885436+timothyolano@users.noreply.github.com>
2022-09-30 09:51:47 +0800

    Add files via upload

---
+++ demo1.php
@@ -0,0 +1,34 @@
+<html> 
+<head><title>This is my first webpage </title></head>
+<body bgcolor="red"> 
+<p> akhshfkjaskdfhaskjdfkjaskjdfhkajshdfkhaskhdfsakhjfhsakjhk </p>
+
+<?php
+
+
+$num1 = 6;
+$num2 = 2; 
+$sum = $num1 + $num2;
+echo "<h2>i miss her</h2>";
+$username = "timothy"; /*this is a multi line comment */
+echo $username; // single line comment 
+$count = "17";
+echo "<br>";
+echo $count; 
+echo "<br>";
+echo "6+2=" .$sum;
+echo "<br>";
+echo "timothy".$username;
+$pi = "3.14";
+echo $pi;
+echo "<br>";
+$radius = 5;
+echo $radius;
+echo "<br>";
+"input radius value";
+$circle = $pi * $radius**2;
+echo "the circle area is " .$circle;
+?>
+</body>
+</html> 
+
